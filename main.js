89008e05145c597b770d70dc6d94e2e828d2c249
ThiagoSaviR <thiagosavir@gmail.com>
2021-10-18 23:13:39 -0300

    Ajustes no javascript

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-//CONSTANTES QUE GARDAM O VALOR DO INPUT DAS ATIVIDADES NO HTML E CONVERTE PARA FLOAT
-const atv1 = parseFloat(document.getElementById('a1').value.replace(',', '.'))
-const atv2 = parseFloat(document.getElementById('a2').value.replace(',', '.'))
-const atv3 = parseFloat(document.getElementById('a3').value.replace(',', '.'))
-const atv4 = parseFloat(document.getElementById('a4').value.replace(',', '.'))
+//CONSTANTES QUE GUARDAM O VALOR DO INPUT DAS ATIVIDADES NO HTML.
+const a1 = document.getElementById('a1')
+const a2 = document.getElementById('a2')
+const a3 = document.getElementById('a3')
+const a4 = document.getElementById('a4')
+const p = document.getElementById('prova')
 
-console.log('Notas das atividades = ' + atv1, atv2, atv3, atv4)
-
-const prova = parseFloat(
-  document.getElementById('prova').value.replace(',', '.')
-)
-console.log('Nota da prova = ' + prova)
-
-// CONSTANTE QUE GUARDA O VALOR DO RESULTADO DO HTML 
 const resultado = document.getElementById('resultado')
 
 function calculo() {
-  // FUNÇÃO QUE CALCULA A MÉDIA
+  // CONVERTE A STRING PARA FLOAT E SUBSTITUI A VIRGULA POR PONTO, CASO NECESSÁRIO.
+  const atv1 = parseFloat(a1.value.replace(',', '.'))
+  const atv2 = parseFloat(a2.value.replace(',', '.'))
+  const atv3 = parseFloat(a3.value.replace(',', '.'))
+  const atv4 = parseFloat(a4.value.replace(',', '.'))
+  const prova = parseFloat(p.value.replace(',', '.'))
+
+  console.log('Notas das atividades = ' + atv1, atv2, atv3, atv4)
+  console.log('Nota da prova = ' + prova)
+
+  // CALCULO DA MÉDIA
   const n1 = ((atv1 + atv2 + atv3 + atv4) / 4) * 0.4
   console.log('Total da N1 = ' + n1)
 
@@ -25,7 +28,6 @@ function calculo() {
   const media = n2 + n1
   console.log('Média final =' + media.toFixed(2))
 
-  resultado.innerHTML = media // SUBSTITUI O VALOR DO RESULTADO NO HTML
+  // SUBSTITUI O VALOR DO RESULTADO NO HTML PELO O VALOR DA MÉDIA.
+  resultado.innerHTML = media
 }
-
-calculo()
